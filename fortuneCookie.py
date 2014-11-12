31c5c9031128a86eaa6d1b6cba7745b00f8ff219
mdulieu <mdulieu@gmail.com>
2014-11-12 17:18:02 -0500

    works locally

--- fortuneCookie.py
+++ fortuneCookie.py
@@ -1,12 +1,49 @@
+#send one of 823 Fortune Cookie quotes
+
+from bs4 import BeautifulSoup
 import zulip
 import os
+import random
+import requests
+
+fortunes = []
+for i in range(1,18):
+
+	quotes = open("FortuneCookieQuotes" + str(i) + ".html", "rw")
+	soup = BeautifulSoup(quotes)
+	fortunesTemp = [t.string for t in soup.table.find_all('a')]
+	fortunes += fortunesTemp[1:]
+
+random.seed()
 
 client = zulip.Client(os.environ['ZULIP_EMAIL'], os.environ['ZULIP_KEY']) 
 
-# Send a stream message
-client.send_message({
-    "type": "stream",
-    "to": "bot-test",
-    "subject": "Fortune Cookie",
-    "content": "Meeting adversity well is the source of your strength."
-})
+def subscribe_all():
+	global client
+	response = requests.get('https://api.zulip.com/v1/streams', auth=requests.auth.HTTPBasicAuth(os.environ['ZULIP_EMAIL'], os.environ['ZULIP_KEY']))
+	if response.status_code == 200:
+		json = response.json()['streams']
+		streams = [{'name': stream['name']} for stream in json]
+		client.add_subscriptions(streams)
+	else:
+		raise Exception(response)
+
+def handle_msg(msg):
+	if '@**FortuneCookie**' in msg['content']:
+		index = random.randint(0,len(fortunes)-1)
+		fortune = fortunes[index]
+		client.send_message({
+    	"type": "stream",
+    	#"to": "bot-test",
+    	"to":msg["display_recipient"],
+    	#"subject": "Fortune Cookie",
+    	"subject": msg['subject'],
+    	"content": fortune
+		})
+		
+
+
+#client.add_subscriptions([{'name':'bot-test'}])
+subscribe_all()
+
+client.call_on_each_message(handle_msg)
